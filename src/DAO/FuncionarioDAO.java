1d899492ead36ace16e358b1a42997104f42a788
qGuga <138495900+qGuga@users.noreply.github.com>
2025-07-04 22:43:21 -0300

    FuncionarioDAO.java

--- src/DAO/FuncionarioDAO.java
+++ src/DAO/FuncionarioDAO.java
@@ -1,44 +1,109 @@
-package entity;
+package DAO;
 
-public class Funcionario {
-    private int id;
-    private String nome;
-    private String email;
-    private boolean admin;
-    private String senha;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
+import conection.Conection;
+import entity.Client;
 
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+public class ClientDAO {
+    public void resgisterClient(Client client){
+        String sql = "INSERT INTO cliente (NOME_CLIENTE, CPF_CLIENTE) VALUES (?, ?)";
 
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
+        PreparedStatement ps = null;
+        try {
+            ps = Conection.getConnection().prepareStatement(sql);
 
-    public boolean isAdmin() {
-        return admin;
-    }
-    public void setAdmin(boolean admin) {
-        this.admin = admin;
+            ps.setString(1, client.getName());
+            ps.setString(2, client.getCpf());
+
+            ps.execute();
+            ps.close();
+            
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
-    public String getSenha() {
-        return senha;
+    public List<Client> loadClients () throws SQLException {
+        String sql = "SELECT ID_CLIENTE, NOME_CLIENTE, CPF_CLIENTE FROM cliente";
+        PreparedStatement ps = null;
+        ResultSet result = null;
+        List<Client> clients = new ArrayList<>();
+
+        try {
+            ps = Conection.getConnection().prepareStatement(sql);
+            result = ps.executeQuery();
+            
+            while(result.next()){
+                Client client = new Client();
+                client.setId(result.getInt("ID_CLIENTE"));
+                client.setName(result.getString("NOME_CLIENTE"));
+                client.setCpf(result.getString("CPF_CLIENTE"));
+                clients.add(client);
+            }
+        } finally {
+            if(result != null) result.close();
+            if(ps != null) ps.close();
+        }
+        return clients;
     }
-    public void setSenha(String senha) {
-        this.senha = senha;
+
+    public Client searchClientById(int id) throws SQLException{
+    String sql = "SELECT ID_CLIENTE, NOME_CLIENTE, CPF_CLIENTE FROM cliente WHERE ID_CLIENTE = ?";
+    PreparedStatement ps = null;
+    ResultSet result = null;
+    Client client = null;
+        try{
+            ps = Conection.getConnection().prepareStatement(sql);
+            ps.setInt(1, id);
+
+            result = ps.executeQuery();
+            if(result.next()){
+                client = new Client();
+                client.setId(result.getInt("ID_CLIENTE"));
+                client.setName(result.getString("NOME_CLIENTE"));
+                client.setCpf(result.getString("CPF_CLIENTE"));
+            }
+        } finally {
+            if(result != null) result.close();
+            if(ps != null) ps.close();
+        }
+        return client;
+    }
+
+    public void updateClient(Client client) throws SQLException {
+        String sql = "UPDATE cliente SET NOME_CLIENTE = ?, CPF_CLIENTE = ? WHERE ID_CLIENTE = ?";
+        PreparedStatement ps = null;
+
+        try {
+            ps = Conection.getConnection().prepareStatement(sql);
+            ps.setString(1, client.getName());
+            ps.setString(2, client.getCpf());
+            ps.setInt(3, client.getId());
+
+            ps.executeUpdate();
+            System.out.println("DAO: Cliente ID " + client.getId() + " atualizado.");
+        } finally {
+            if(ps != null) ps.close();
+        }
+    }
+
+    public void deletedClient(Client client) throws SQLException{
+        String sql = "DELETE FROM cliente WHERE ID_CLIENTE = ?";
+        PreparedStatement ps = null;
+        try{
+            ps = Conection.getConnection().prepareStatement(sql);
+            ps.setInt(1, client.getId());
+
+            ps.executeUpdate();
+            System.out.println("DAO: cliente com o id " + client.getId() + " deletado");
+        } finally {
+            if(ps != null) ps.close();
+        }
     }
 }
