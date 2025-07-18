688a1774fe491e4c8e42e6d638c2b735c9500bb8
Maur1c1oPJr <95836867+Maur1c1oPJr@users.noreply.github.com>
2025-07-18 20:10:44 -0300

    Update SaleService.java
    funcao criar venda

--- src/services/SaleService.java
+++ src/services/SaleService.java
@@ -1,5 +1,23 @@
 package services;
 
+import DAO.SaleDAO;
+import entity.Sale;
+import java.util.Date;
+
 public class SaleService {
-    // Criar função que cria uma venda
+    private SaleDAO saleDAO;
+
+    public SaleService(SaleDAO saleDAO) {
+        this.saleDAO = saleDAO;
+    }
+
+    public void createSale(int idCliente, int idFuncionario) {
+        Sale sale = new Sale();
+        sale.setDate(new Date());
+        sale.setIdCliente(idCliente);
+        sale.setIdFuncionario(idFuncionario);
+
+        saleDAO.resgisterSale(sale);
+        System.out.println("Venda criada com sucesso.");
+    }
 }
